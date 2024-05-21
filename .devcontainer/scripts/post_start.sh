84337986fa80f17833ce2c5fe61a5950c05b1825
Tomoki Ota <tomo.soccer1227@gmail.com>
2024-05-22 01:56:50 +0900

    feat ruff & peotry (#1)
    * feat🎉 ruffを導入する
    
    * build👷 ruffの設定を修正
    
    * build👷 poetryを導入
    
    * test dependabot

--- .devcontainer/scripts/post_start.sh
+++ .devcontainer/scripts/post_start.sh
@@ -1,6 +1,13 @@
 # #  ~/.gitconfigの設定
-# echo "start to copy local ~/.gitconfig"
-# git config --global --add safe.directory $1
+echo "start to copy local ~/.gitconfig"
+git config --global --add safe.directory $1
 
-# git config pull.rebase false
-# git config --global commit.template .commit_template
+git config pull.rebase false
+git config --global commit.template .commit_template
+
+pip install --no-cache-dir --upgrade pip
+pip install --no-cache-dir poetry
+
+poetry config virtualenvs.create false
+
+poetry install
