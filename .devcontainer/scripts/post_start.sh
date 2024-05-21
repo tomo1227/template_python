f73f51398255b7e2850a05991e9bb1b287304f01
tomo1227 <tomo.soccer1227@gmail.com>
2024-05-21 16:50:38 +0000

    build👷 poetryを導入

--- .devcontainer/scripts/post_start.sh
+++ .devcontainer/scripts/post_start.sh
@@ -6,13 +6,8 @@ git config pull.rebase false
 git config --global commit.template .commit_template
 
 pip install --no-cache-dir --upgrade pip
+pip install --no-cache-dir poetry
 
-REQUIREMENTS_FILE="../requirements.txt"
+poetry config virtualenvs.create false
 
-# ファイルが存在するかどうかを確認
-if [ -f "$REQUIREMENTS_FILE" ]; then
-    echo "ファイルが存在します: $REQUIREMENTS_FILE"
-    pip install --no-cache-dir -r "$REQUIREMENTS_FILE"
-else
-    echo "ファイルが存在しません: $REQUIREMENTS_FILE"
-fi
+poetry install
